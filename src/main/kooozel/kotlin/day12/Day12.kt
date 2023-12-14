ef2889a6e94d38d73f203142543c5e25d45d7801
Frantisek Sikula <frantisek.sikula@3pillarglobal.com>
2023-12-14 08:35:08 +0100

    Day 12 fun

--- src/main/kooozel/kotlin/day12/Day12.kt
+++ src/main/kooozel/kotlin/day12/Day12.kt
@@ -2,117 +2,109 @@ package main.kooozel.kotlin.day12
 
 import com.google.common.cache.CacheBuilder
 import main.kooozel.kotlin.Day
+import java.util.concurrent.TimeUnit
 
 class Day12 : Day("12") {
     val input = inputListTest
     override fun partOne(): Any {
+        var output = 0
+        // Iterate over each row in the file
+//        for (entry in input) {
+//
+//            // Split into the record of .#? record and the 1,2,3 group
+//            val (record, rawGroups) = entry.split(" ")
+//
+//            // Convert the group from string 1,2,3 into a list
+//            val groups = rawGroups.split(',').map { it.toInt() }
+//
+//            output += calc(record, groups)
+//
+//            // Create a nice divider for debugging
+//            println( "-".repeat(10))
+//        }
         return output
     }
 
 
-    private var output = 0L
+    fun calc(record: String, groups: List<Int>): Int {
 
-    private fun calc(key: Pair<String, List<Int>>): Int {
-        var out = cache.getIfPresent(key)
-        if (out != null) {
-            return out
-        } else {
-            // Did we run out of groups? We might still be valid
-            val groups = key.second
-            val record = key.first
-            if (groups.isEmpty()) {
-                // Make sure there aren't anymore damaged springs, if so, we're valid
-                if ('#' !in record) {
-                    // This will return true even if record is empty, which is valid
+        fun pound(): Int {
+            val thisGroupLength = minOf(record.length, groups[0])
+            val thisGroup = record.substring(0, thisGroupLength).replace("?", "#")
+
+
+            if (thisGroup != "#".repeat(groups[0])) {
+                return 0
+            }
+
+            if (record.length == thisGroupLength) {
+                if (groups.size == 1) {
                     return 1
                 } else {
-                    // More damaged springs that aren't in the groups
                     return 0
                 }
             }
 
-            // There are more groups, but no more record
-            if (record.isEmpty()) {
-                // We can't fit, exit
-                return 0
-            }
-            // Look at the next element in each record and group
-            val nextCharacter = record[0]
-            val nextGroup = groups[0]
-
-            //         Logic that treats the first character as pound-sign "#"
-            fun pound(): Int {
-
-                //If the first is a pound, then the first n characters must be
-                //able to be treated as a pound, where n is the first group number
-                val thisGroup = record[nextGroup].toString().replace("?", "#")
-
-                //If the next group can't fit all the damaged springs, then abort
-                if (thisGroup != "#".repeat(nextGroup)) {
-                    return 0
+            if (record[groups[0]] in "?.") {
+                return cache.get(Pair(record.substring(groups.first() + 1), groups.drop(1))) {
+                    calc(record.substring(groups.first() + 1), groups.drop(1))
                 }
-
-                //If the rest of the record is just the last group, then we're
-                //done and there's only one possibility
-                if (record.length == nextGroup) {
-                    //Make sure this is the last group
-                    if (groups.size == 1) {
-                        // We are valid
-                        return 1
-                    } else {
-                        //There's more groups, we can't make it work
-                        return 0
-                    }
-                }
-
-                // Make sure the character that follows this group can be a seperator
-                if (record[nextGroup] in "?.") {
-                    //It can be seperator, so skip it and reduce to the next group
-                    return calc(Pair(record.substring(nextGroup + 1), groups.drop(1)))
-                }
-                //Can't be handled, there are no possibilites
-                return 0
             }
 
-            //     Logic that treats the first character as dot "."
-            fun dot(): Int {
-                // ADD LOGIC HERE ... need to process this character and call
-                // calc() on a substring
-                return calc(Pair(record.substring(1), groups))
-            }
-
-            if (nextCharacter == '#') {
-                //Test pound logic
-                out = pound()
-            } else if (nextCharacter == '.') {
-                //Test dot logic
-                out = dot()
-            } else if (nextCharacter == '?') {
-                //This character could be either character, so we'll explore both
-                //possibilities
-                out = dot() + pound()
-            } else {
-                throw RuntimeException()
-            }
-
-            cache.put(key, out)
-
-            // Help with debugging
-            println("$record $groups -> $out")
-            return out
+            return 0
         }
+
+        fun dot(): Int {
+            return cache.get(Pair(record.substring(1), groups)) {
+                calc(record.substring(1), groups)
+            }
+        }
+
+        if (groups.isEmpty()) {
+            if ('#' !in record) {
+                return 1
+            } else {
+                return 0
+            }
+        }
+
+        if (record.isEmpty()) {
+            return 0
+        }
+
+        val nextCharacter = record[0]
+
+        val out = when (nextCharacter) {
+            '#' -> pound()
+            '.' -> dot()
+            '?' -> dot() + pound()
+            else -> throw RuntimeException()
+        }
+
+        println("$record $groups $out")
+        return out
     }
 
-    val cache = CacheBuilder.newBuilder().build<Pair<String, List<Int>>, Int>()
+    val cache = CacheBuilder.newBuilder()
+        .maximumSize(Long.MAX_VALUE)
+        .build<Pair<String, List<Int>>, Int>()
 
     override fun partTwo(): Any {
+        var output = 0L
+        // Iterate over each row in the file
         for (entry in input) {
+
+            // Split into the record of .#? record and the 1,2,3 group
             val (record, rawGroups) = entry.split(" ")
+
+            // Convert the group from string 1,2,3 into a list
             val groups = rawGroups.split(',').map { it.toInt() }
-            output += calc(Pair(record, groups))
+
+            output += calc(record.repeat(5), List(5) { groups }.flatten())
+
+            // Create a nice divider for debugging
             println("-".repeat(10))
         }
-
         return output
     }
 
